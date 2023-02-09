1c48a8f9191c20d47274fbc1d4822c2c4cfb970e
haanhtuan211 <117570101+haanhtuan211@users.noreply.github.com>
2023-02-09 11:03:02 +0700

    Create main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests as r,threading
 dem=0
 def send():
-	dem=dem+1
-	response = r.get('https://anhkhoadev.site/grap/index.php?id=49').json()
-	print(f'{dem} - {response}')
+	while True:
+		dem=dem+1
+		response = r.get('https://anhkhoadev.site/grap/index.php?id=49').json()
+		print(f'{dem} - {response}')
 for o in range(100):
 	threading.Thread(target=send).start()
